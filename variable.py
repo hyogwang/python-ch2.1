bfdd6a575417e50e0618d0d847cb942ad165a404
hyogwang <gyrhkd0707@naver.com>
2018-12-10 16:10:21 +0900

    Initial commit

---
+++ variable.py
@@ -0,0 +1,25 @@
+
+import keyword
+
+friend=1
+a =10
+my_name ='안효광'
+myname = '안효광'
+_yourName ='둘리'
+member1 ='도우너'
+
+
+# 에러: 다른 구성의 변수 이름은 사용할 수 없다.
+
+#friend$=1
+#a!=10
+#@myname='안효광'
+#1abc = 10
+#def = 10
+
+print(keyword.kwlist)
+print(len(keyword.kwlist))
+
+#한글이름의 변수도 가능하다.
+가격1 =2000
+print(가격1)
